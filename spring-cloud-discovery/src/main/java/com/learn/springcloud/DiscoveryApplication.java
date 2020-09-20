9fbd6b1573f605b4cfd46a50b626258672c2c83a
zhangyu gu <zhangyugu@zhangyudeMacBook-Pro.local>
2020-09-20 08:49:40 +0800

    网关测试代码

--- spring-cloud-discovery/src/main/java/com/learn/springcloud/DiscoveryApplication.java
+++ spring-cloud-discovery/src/main/java/com/learn/springcloud/DiscoveryApplication.java
@@ -1,13 +1,19 @@
 package com.learn.springcloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * @Author zhangyugu
  * @Date 2020/9/19 4:21 上午
  * @Version 1.0
  */
+@EnableDiscoveryClient
+@SpringBootApplication
 public class DiscoveryApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(DiscoveryApplication.class, args);
     }
 }
---
+++ spring-cloud-discovery/src/main/java/com/learn/springcloud/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.learn.springcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author zhangyugu
+ * @Date 2020/9/17 5:54 上午
+ * @Version 1.0
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("test1")
+    public String test() {
+        return "test1";
+    }
+}
